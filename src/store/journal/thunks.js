e285a7579f54ce56261d812c579d8f5adf945804
Carlos Arias <ceau922@gmail.com>
2025-03-23 18:50:21 -0500

    Revisando notas al iniciar sesion

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,7 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
-import { addNewEmptyNote, savingNewNote, setActiveNote } from "./journalSlice";
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from "./journalSlice";
+import { loadNotes } from "../../helpers";
 
 
 export const startNewNote = () => {
@@ -26,6 +27,21 @@ export const startNewNote = () => {
         dispatch( addNewEmptyNote( newNote ) );
         dispatch( setActiveNote( newNote ) );
         
+    }
+};
+
+
+
+export const startLoadingNotes = () => {
+    return async( dispatch, getState ) => {
+
+        const { uid } = getState().auth; 
+        if( !uid ) throw new Error("El id del usuario no está establecido");
+
+        const notes = await loadNotes( uid );
+
+        dispatch( setNotes( notes ) );  
+
     }
 }
 
