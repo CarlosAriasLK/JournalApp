f6eaa9a6b6dd96a04cc7bf590f220bc89901a2fa
Carlos Arias <ceau922@gmail.com>
2025-03-24 21:15:39 -0500

    Actualizacion de la nota

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,11 +1,14 @@
-import { SaveOutlined } from "@mui/icons-material"
-import { Button, Grid, TextField, Typography } from "@mui/material"
-import { ImageGallery } from "../components"
-import { useForm } from "../../hooks/useForm"
-import { useSelector } from "react-redux"
-import { useMemo } from "react"
+import { SaveOutlined } from '@mui/icons-material';
+import { Button, Grid, TextField, Typography } from '@mui/material';
+import { ImageGallery } from '../components';
+import { useForm } from '../../hooks/useForm';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useMemo } from 'react';
+import { setActiveNote, startSaveNote } from '../../store/journal';
 
 export const NoteView = () => {
+
+    const dispatch = useDispatch();
 
     const { active: note } = useSelector( state => state.journal );
 
@@ -13,10 +16,17 @@ export const NoteView = () => {
 
     const dateString = useMemo( () => {
         const newDate = new Date( date )
-
         return newDate.toUTCString();
-
     }, [date] );
+
+    useEffect(() => {
+        dispatch( setActiveNote( formState ) );
+    }, [formState])
+
+    
+    const onSaveNote = () => {
+        dispatch( startSaveNote() );  
+    }
 
 
     return (
@@ -33,7 +43,11 @@ export const NoteView = () => {
             </Grid>
 
             <Grid item>
-                <Button color="primary" sx={{ padding: 2 }} >
+                <Button 
+                    onClick={ onSaveNote }
+                    color="primary" 
+                    sx={{ padding: 2 }} 
+                >
                     <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
                     Guardar
                 </Button>
