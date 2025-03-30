151f1cb695789896f96210d609fe6c873b2b90a6
Carlos Arias <ceau922@gmail.com>
2025-03-29 21:13:40 -0500

    Usando fetchApi para subir imagenes a cloudinary

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -8,7 +8,7 @@ import 'sweetalert2/dist/sweetalert2.css';
 
 import { ImageGallery } from '../components';
 import { useForm } from '../../hooks/useForm';
-import { setActiveNote, startSaveNote } from '../../store/journal';
+import { setActiveNote, startSaveNote, startUploadingFiles } from '../../store/journal';
 
 
 export const NoteView = () => {
@@ -44,8 +44,7 @@ export const NoteView = () => {
     const onFileInputChange = ({ target }) => {
         if ( target.files === 0 ) return;
 
-        console.log("subinedo Archivos");
-        /* dispatch( startUploadingFiles( target.files )); */
+        dispatch( startUploadingFiles( target.files ));
     }
 
 
