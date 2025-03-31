5a21faa0f8f3fa23a21a807f89a68902972d1d69
Carlos Arias <ceau922@gmail.com>
2025-03-30 19:32:25 -0500

    Funcionalidad eliminar notas

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,6 @@
-import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { collection, deleteDoc, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
-import { addNewEmptyNote, savingNewNote, setActiveNote, setFotosToActiveNote, setNotes, setSaving, updateNote } from "./journalSlice";
+import { addNewEmptyNote, deleteNoteById, savingNewNote, setActiveNote, setFotosToActiveNote, setNotes, setSaving, updateNote } from "./journalSlice";
 import { fileUpload, loadNotes } from "../../helpers";
 
 
@@ -76,9 +76,25 @@ export const startUploadingFiles = ( files = [] ) => {
 
         const photosUrls = await Promise.all( fileUploadPromises );
         
-        dispatch(  setFotosToActiveNote( photosUrls ) );
+        dispatch( setFotosToActiveNote( photosUrls ) );
 
     }
 };
+
+
+export const startDeletingNote = () => {
+    return async( dispatch, getState ) => {
+        
+        const { uid } = getState().auth;
+        const { active:note } = getState().journal;
+
+
+        const docRef = doc( FirebaseDB, `${ uid }/journal/notes/${ note.id }` );
+        await deleteDoc( docRef );
+
+        dispatch( deleteNoteById( note.id ) );
+
+    }
+}
 
 
