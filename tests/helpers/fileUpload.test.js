d3daf2b79de9e150d85bda7d9b0b05b040054a3f
Carlos Arias <ceau922@gmail.com>
2025-03-31 19:21:06 -0500

    Pruebas en fileUpload

--- tests/helpers/fileUpload.test.js
+++ tests/helpers/fileUpload.test.js
@@ -1,5 +1,12 @@
+import { v2 as cloudinary } from 'cloudinary';
 import { fileUpload } from "../../src/helpers/fileUpload";
 
+cloudinary.config({
+    cloud_name: 'ddiy604gb',
+    api_key: '391564884684438',
+    api_secret: 'IU1HE7xduzEuiGJaJaLObtBDb2I',
+    secure: true
+});
 
 describe('Pruebas en fileUpload', () => {
 
@@ -10,12 +17,27 @@ describe('Pruebas en fileUpload', () => {
         const resp = await fetch( imageUrl );
         const blob = await resp.blob();
         const file = new File( [blob], 'foto.jpg' );
-        console.log(file);
-
         /* const url = await fileUpload( file ); */
 
         expect( typeof url ).toBe( 'string' );
 
+        const segments = url.split('/');
+        const imageId = segments[ segments.length - 1 ].replace('.jpg', '');
+        const cloudResp = await cloudinary.api.delete_resources([ `journal/${ imageId }` ], {
+            resource_type: 'image'
+        });
+
+        console.log({cloudResp})
+
     });
+
+    test('Debe de retornar null', async() => {
+
+        const file = new File( [], 'foto.jpg' );
+        const url = await fileUpload( file );
+        expect( url ).toBe( null );
+    });
+
+
 
 });
