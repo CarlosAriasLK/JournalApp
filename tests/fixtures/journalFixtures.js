0b34dac7c26e7fb79589135c59764ecbcdc9c013
Carlos Arias <ceau922@gmail.com>
2025-04-07 20:41:47 -0500

    Terminacion de las pruebas en journalSlice

--- tests/fixtures/journalFixtures.js
+++ tests/fixtures/journalFixtures.js
@@ -5,11 +5,45 @@ export const initialState = {
     active: null,
 };
 
-
 export const emptyNote = {
     title: '',
     body: '',
     date: '1744074844977',
     id: 'TEST-ID'
 };
+
+export const note = {
+    title: 'Duro',
+    body: 'El mas fresa pa',
+    date: '1744074844977',
+    id: 'TEST-ID'
+};
+
+
+export const notes = [
+    {
+        title: 'Duro',
+        body: 'El mas fresa pa',
+        date: '1744074844977',
+        id: 'TEST-ID'
+    },
+    {
+        title: 'el mas duro',
+        body: 'El mas',
+        date: '1744074844977',
+        id: 'TEST-ID2'
+    },
+    {
+        title: 'Duro casi',
+        body: 'El mas fresa pa',
+        date: '1744074844977',
+        id: 'TEST-ID3'
+    },
+    {
+        title: 'Duro duro',
+        body: 'El mas fresa pa',
+        date: '1744074844977',
+        id: 'TEST-ID4'
+    },
+];
 
--- tests/store/journal/journalSlice.test.js
+++ tests/store/journal/journalSlice.test.js
@@ -1,5 +1,5 @@
-import { addNewEmptyNote, journalSlice, savingNewNote } from "../../../src/store/journal/journalSlice";
-import { emptyNote, initialState } from "../../fixtures/journalFixtures";
+import { addNewEmptyNote, journalSlice, savingNewNote, setActiveNote, setNotes } from "../../../src/store/journal/journalSlice";
+import { emptyNote, initialState, note, notes } from "../../fixtures/journalFixtures";
 
 describe('Pruebas en el journalSlice', () => {
 
@@ -12,19 +12,18 @@ describe('Pruebas en el journalSlice', () => {
 
     });
 
+
     test('debe de guardar la nueva nota', () => {
 
         const state = journalSlice.reducer( initialState, savingNewNote() );
-
         expect( state.isSaving ).toBe( true );
 
     });
 
+
     test('debe de añadir una nota vacia', () => {
 
         const state = journalSlice.reducer( initialState, addNewEmptyNote( emptyNote ) )
-
-        console.log(state)
 
         expect( state ).toEqual({
             isSaving: false,
@@ -37,6 +36,63 @@ describe('Pruebas en el journalSlice', () => {
             }],
             active: null
         })
+
+    });
+
+    test('debe de establecer una nota activa', () => {
+
+        const state = journalSlice.reducer( initialState, setActiveNote( note ));
+
+        expect( state ).toEqual({
+            isSaving: false,
+            messageSaved: '',
+            notes: [],
+            active: {
+                title: note.title,
+                body: note.body,
+                date: note.date,
+                id: note.id,
+            }
+        })
+
+    });
+
+
+    test('debe establecer las notas', () => {
+
+        const state = journalSlice.reducer( initialState, setNotes( notes ) );
+
+        expect( state ).toEqual({
+            isSaving: false,
+            messageSaved: '',
+            notes: [
+              {
+                title: 'Duro',
+                body: 'El mas fresa pa',
+                date: '1744074844977',
+                id: 'TEST-ID'
+              },
+              {
+                title: 'el mas duro',
+                body: 'El mas',
+                date: '1744074844977',
+                id: 'TEST-ID2'
+              },
+              {
+                title: 'Duro casi',
+                body: 'El mas fresa pa',
+                date: '1744074844977',
+                id: 'TEST-ID3'
+              },
+              {
+                title: 'Duro duro',
+                body: 'El mas fresa pa',
+                date: '1744074844977',
+                id: 'TEST-ID4'
+              }
+            ],
+            active: null
+          });
 
     });
 
