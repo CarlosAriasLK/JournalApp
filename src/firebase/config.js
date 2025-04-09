8e5dbecbdd9ed7939523ca6ab46e14418937bb1b
Carlos Arias <ceau922@gmail.com>
2025-04-08 20:09:54 -0500

    Variables de entorno correctamente configuradas

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,30 +8,25 @@ import { getEnvironments } from '../helpers/getEnvironments';
 
 // Your web app's Firebase configuration
 
-const env = getEnvironments()
-console.log( env )
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID,
+} = getEnvironments();
 
 //console.log( import.meta.env ) // Funciona en desarrollo pero no en testing
 //console.log( process.env ) // Funciona en testing pero no en desarrollo
 
-// DEV/Prod
-/* const firebaseConfig = {
-  apiKey: "AIzaSyDrU7ndRd676dZeh1gjfQh4UU-1v8ewSqU",
-  authDomain: "journalapp-39e8b.firebaseapp.com",
-  projectId: "journalapp-39e8b",
-  storageBucket: "journalapp-39e8b.firebasestorage.app",
-  messagingSenderId: "607957120698",
-  appId: "1:607957120698:web:421476d0748c3945c63483"
-}; */
-
-// Testing
 const firebaseConfig = {
-  apiKey: "AIzaSyDfAtjpxQJiz9sxDOxJpkeN1J2gKyY1uZU",
-  authDomain: "test-react-journal-48a69.firebaseapp.com",
-  projectId: "test-react-journal-48a69",
-  storageBucket: "test-react-journal-48a69.firebasestorage.app",
-  messagingSenderId: "431383830827",
-  appId: "1:431383830827:web:a1053122f473dcc9f0256e"
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID,
 };
 
 // Initialize Firebase
